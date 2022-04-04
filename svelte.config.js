857f3a32bf90727658dcc94a0a676facca1c171a
eur2 <gregory.dapra@gmail.com>
2022-04-04 11:10:15 +0200

    pkg update and adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
